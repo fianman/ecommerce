05593c9288d894e400b0efa4090c12b7a8f53f11
FroZEn1305 <tmarsena@gmail.com>
2018-07-23 16:40:34 +0700

    Login new look

--- ecommerce_react/src/app/components/login/FormLogin.js
+++ ecommerce_react/src/app/components/login/FormLogin.js
@@ -1,14 +1,14 @@
 
 import React from 'react';
-import { Input, Button, Col, Fa, CardTitle } from 'mdbreact';
+import { Container, Row, ModalFooter, Card, CardBody, Input, Button, Col, Fa } from 'mdbreact';
 import FormErrors from './FormErrors';
 
 class FormLogin extends React.Component {
   constructor(props){
     super(props);
-    this.state = {     
+    this.state = {
       email: '',
-      password: '',    
+      password: '',
       formErrors: {email: '', password: ''},
       emailValid: false,
       passwordValid: false,
@@ -19,7 +19,7 @@ class FormLogin extends React.Component {
   handleUserInput (e) {
     const name = e.target.name;
     const value = e.target.value;
-    this.setState({[name]: value}, 
+    this.setState({[name]: value},
                   () => { this.validateField(name, value) });
   }
 
@@ -61,39 +61,54 @@ class FormLogin extends React.Component {
 
   render(){
     return(
-      <div className="container-fluid">
-        <Col className="col-md-12">
-          <CardTitle>      
-          <legend className="text-center mb-4">Form Sign In</legend> 
-            <FormErrors formErrors={this.state.formErrors} />
-            <form method="post" action="/auth/login">
-              <Input name="email" className={`md-form ${this.errorClass(this.state.formErrors.email)}`} label="Type your email" icon="envelope" group type="email" value={this.state.email} onChange={(event) => this.handleUserInput(event)}/>
-              <Input name="password" className={`md-form ${this.errorClass(this.state.formErrors.password)}`} label="Type your password" icon="lock" group type="password" value={this.state.password} onChange={(event) => this.handleUserInput(event)}/>
-              <div className="text-center">
-                <Button type="submit" disabled={!this.state.formValid}>Login</Button>  
-                <a href="/register" >Make An Account</a> 
-              </div>
-              
-              <div className="modal-footer">
-                <div className="options font-weight-light">
-                    <p>Forgot <a href="/auth/forgot">Password?</a></p>
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col md="6">
+            <Card>
+              <CardBody>
+                <FormErrors formErrors={this.state.formErrors} />
+                <form method="post" action="/auth/login">
+                  <p className="h3 text-center py-4"><strong>Login</strong></p>
+                  <div className="grey-text">
+                    <Input name="email"
+                      className={`md-form ${this.errorClass(this.state.formErrors.email)}`}
+                      label="Type your email" icon="envelope" group type="email"
+                      value={this.state.email}
+                      onChange={(event) => this.handleUserInput(event)} />
+                    <Input name="password"
+                      className={`md-form ${this.errorClass(this.state.formErrors.password)}`}
+                      label="Type your password" icon="lock" group type="password"
+                      value={this.state.password}
+                      onChange={(event) => this.handleUserInput(event)} />
+                  </div>
+                  <div className="text-center py-4 mt-3">
+                    <Button color="cyan" disabled={!this.state.formValid} type="submit">
+                      <strong>Login</strong>
+                    </Button>
+                    <p className="font-small blue-text d-flex justify-content-end pb-3">Forgot <a href="/auth/forgot" className="blue-text ml-1"> Password?</a></p>
+                  </div>
+                  <p className="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-2"> or Sign in with:</p>
+                </form>
+                <div className="text-center">
+                  <Button type="button" href="/auth/instagram" color="pink" className="mr-md-3 z-depth-1a">
+                    <Fa icon="instagram" className="white-text text-center" />
+                    <strong> Instagram</strong>
+                  </Button>
+                  <Button type="button" href="/auth/google" color="red" className="z-depth-1a">
+                    <Fa icon="google-plus" className="white-text" />
+                    <strong> Google</strong>
+                  </Button>
                 </div>
-              </div>
-              
-              <div className="text-center">
-                <Button color="pink" href="/auth/instagram">
-                  <Fa icon="instagram" className="mr-3"/>
-                  Sign in with Instagram
-                </Button>
-                <Button color="danger" href="/auth/google">
-                  <Fa icon="google" className="mr-3"/>
-                  Sign in with Google
-                </Button>
-              </div>
-            </form>
-          </CardTitle>
-        </Col>
-      </div>
+                <ModalFooter className="mx-5 pt-3 mb-1">
+                  <p className="font-small grey-text d-flex justify-content-end">Not a member? <a href="/register" className="blue-text ml-1"> Sign Up</a></p>
+                </ModalFooter>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
     );
   }
 }
