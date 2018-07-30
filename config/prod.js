b6abd9e632040192e61324f0fe51faf70b7fa616
fianman <fianpress@gmail.com>
2018-07-30 11:32:19 +0700

    carousel

--- config/prod.js
+++ config/prod.js
@@ -3,21 +3,25 @@ module.exports = {
 	// google
 	googleClientID : process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+
 	// instagram
 	iyClientID : process.env.IY_CLIENT_ID,
 	iyClientSecret : process.env.IY_CLIENT_SECRET,
+
 	// mongo
 	mongoURI : process.env.MONGO_URI,
 	cookieKey: process.env.COOKIE_KEY,
+
 	// stripe
-	stripePublishableKey : process.env.STRIPE_PUBLISHABLE_KEY,
-	stripeSecretKey : process.env.STRIPE_SECRET_KEY,
-	sendGridKey: process.env.SEND_GRID_KEY,
 	redirectDomain: process.env.REDIRECT_DOMAIN,
 
 	// mid trans
 	serverKeyDev: process.env.SERVER_KEY_DEV,
 	clientKeyDev: process.env.CLIENT_KEY_DEV,
 	sendboxDev: process.env.SENDBOX_DEV,
-	sendboxProd: process.env.SENDBOX_PROD
+	sendboxProd: process.env.SENDBOX_PROD,
+
+	// facebook
+	facebookID: process.env.FACEBOOK_ID,
+	facebookSecret: process.env.FACEBOOK_SECRET
 };
