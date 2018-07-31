283dba725c8e83cd430549dad7d1f75e0812fa73
FroZEn1305 <tmarsena@gmail.com>
2018-07-31 10:39:53 +0700

    Fix Carousel

--- ecommerce_react/src/app/components/basics/CarouselPage.js
+++ ecommerce_react/src/app/components/basics/CarouselPage.js
@@ -4,7 +4,7 @@ import  {Carousel, CarouselCaption, CarouselInner, CarouselItem, View, Mask, Con
 class CarouselPage extends Component {
   render(){
     return(
-      <Container className="carousel slide carousel-fade crs-zidx">
+      <Container>
         <Carousel
           activeItem={1}
           length={4}
