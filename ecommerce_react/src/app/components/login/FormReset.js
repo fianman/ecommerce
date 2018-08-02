95045e4246174cabba101f32b40dda7a949ec092
fianman <fianpress@gmail.com>
2018-08-02 12:46:20 +0700

    add icons eyes + functoins

--- ecommerce_react/src/app/components/login/FormReset.js
+++ ecommerce_react/src/app/components/login/FormReset.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Col, CardTitle, Input, Button} from 'mdbreact';
+import {Col, CardTitle, Input, Button, Fa, Row} from 'mdbreact';
 import FormErrors from './FormErrors';
 
 class FormReset extends React.Component {
@@ -11,7 +11,9 @@ class FormReset extends React.Component {
       formErrors: {password: '', password2: ''},
       passwordValid: false,
       password2Valid: false,
-      formValid: false
+      formValid: false,
+      visible1: false,
+      visible2: false
     }
   }
 
@@ -50,18 +52,54 @@ class FormReset extends React.Component {
     this.setState({formValid: this.state.passwordValid && this.state.password2Valid});
   }
 
+  changeVisibility1() {
+    const { visible1 } = this.state
+    this.setState({ visible1: !visible1 })
+  }
+  changeVisibility2() {
+    const { visible2 } = this.state
+    this.setState({ visible2: !visible2 })
+  }
+  eyeIcon1() {
+    if (!this.state.visible1){ return "eye-slash" }
+    return "eye"
+  }
+  eyeIcon2() {
+    if (!this.state.visible2){ return "eye-slash" }
+    return "eye"
+  }
+  passwordType1() {
+    if (!this.state.visible1) { return "password" }
+    return "text"
+  }
+  passwordType2() {
+    if (!this.state.visible2) { return "password" }
+    return "text"
+  }
     render(){
-      console.log('token :', this.props.match);
         return(       	
          <Col  md="6" className="col-md-6">
             <CardTitle>
               Reset Password
               <hr/>   
               <FormErrors formErrors={this.state.formErrors} />
-              <form method="post" action={`/reset/${this.props.match.params.token}`}>
-                
-                <Input name="password" label="New Password" icon="lock" group type="password" value={this.state.password} onChange={(event) => this.handleUserInput(event)}/>
-                <Input name="password2" label="Confirm Password" icon="lock" group type="password" value={this.state.password2} onChange={(event) => this.handleUserInput(event)}/>
+              <form method="post" action={`/reset/${this.props.match.params.token}`}>   
+                <Row>  
+                  <Col className="col-md-10">     
+                    <Input name="password" label="New Password" icon="lock" group type={this.passwordType1()} value={this.state.password} onChange={(event) => this.handleUserInput(event)}/>
+                  </Col>
+                  <Col className="col-md-2"> 
+                    <Fa icon={this.eyeIcon1()} onClick={this.changeVisibility1.bind(this)} className="icon-eyes"/>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="col-md-10">  
+                    <Input name="password2" label="Confirm Password" icon="lock" group type={this.passwordType2()} value={this.state.password2} onChange={(event) => this.handleUserInput(event)}/>
+                  </Col>
+                  <Col className="col-md-2"> 
+                    <Fa icon={this.eyeIcon2()} onClick={this.changeVisibility2.bind(this)} className="icon-eyes"/>
+                  </Col>
+                </Row>
                 <div className="text-center">
                   <Button disabled={!this.state.formValid} type="submit">Update Password</Button>
                 </div>
